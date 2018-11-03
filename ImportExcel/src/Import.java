6a6dd551bb828fd35fd99b519eff9484ef4e0dad
yaswanth raja <yaswanth.raja@aeione.com>
2018-11-03 10:34:19 +0530

    reading excel sheet base code

---
+++ ImportExcel/src/Import.java
@@ -0,0 +1,26 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+public class Import {
+	
+	public static void readFromExcel(String file) throws IOException
+	{
+		System.out.println(file);
+		XSSFWorkbook myExcelBook = new XSSFWorkbook(new FileInputStream(file));
+		XSSFSheet myExcelSheet = myExcelBook.getSheet("medicine");
+		XSSFRow row = myExcelSheet.getRow(0);
+		System.out.println(row);
+		myExcelBook.close(); 
+	}
+
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		readFromExcel("C://Users/Dell/Downloads/Medicine_Database.xlsx");
+	}
+
+}
